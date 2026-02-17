5443f1db8b01983a0c80e724a554a52aa8d13994
nely <candezmr2@gmail.com>
2026-02-17 17:47:19 +0100

    eliminando comentarios

--- apps/frontend/src/pages/CuestionarioPersonal.tsx
+++ apps/frontend/src/pages/CuestionarioPersonal.tsx
@@ -7,14 +7,10 @@ const CuestionarioPersonal: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    // Layout envuelve todo (fondo oscuro)
     <Layout>
-      // Un div para centrar el contenido
       <div>
-        // Título
         <h1>Cuestionario Personal</h1>
         
-        // El botón que navega
         <Button onClick={() => navigate("/cuestionario-salud")}>
           Siguiente
         </Button>
--- apps/frontend/src/pages/CuestionarioSalud.tsx
+++ apps/frontend/src/pages/CuestionarioSalud.tsx
@@ -7,19 +7,19 @@ const CuestionarioSalud: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    // Layout envuelve todo (fondo oscuro)
+    
     <Layout>
-      // Un div para centrar el contenido
+      
       <div>
-        // Título
+        
         <h1>Cuestionario Personal</h1>
         
-        // El botón que navega
+        
         <Button onClick={() => navigate("/cuestionario-personal")}>
           Siguiente
         </Button>
 
-        // El botón que finaliza
+        
         <Button onClick={() => navigate("/dashboard")}>
           Finalizar
         </Button>
