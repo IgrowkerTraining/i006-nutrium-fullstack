8d6bce74650b676aafb44ad9f92d528cce891093
nely <candezmr2@gmail.com>
2026-02-17 17:17:30 +0100

    creo CuestionarioSalud.tsx

---
+++ apps/frontend/src/pages/CuestionarioSalud.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "../components/common/Button";
+import { Layout } from "../components/layout/Layout";
+
+const CuestionarioSalud: React.FC = () => {
+  const navigate = useNavigate();
+
+  return (
+    // Layout envuelve todo (fondo oscuro)
+    <Layout>
+      // Un div para centrar el contenido
+      <div>
+        // Título
+        <h1>Cuestionario Personal</h1>
+        
+        // El botón que navega
+        <Button onClick={() => navigate("/cuestionario-salud")}>
+          Siguiente
+        </Button>
+
+        // El botón que finaliza
+        <Button onClick={() => navigate("/dashboard")}>
+          Finalizar
+        </Button>
+
+
+      </div>
+    </Layout>
+
+  );
+};
+
+export default CuestionarioSalud;
