5973bf2ea6cee3184b9817342065372b72f01982
nely <candezmr2@gmail.com>
2026-02-20 16:43:51 +0100

    add TerminosYCondiciones.tsx y sus rutas

---
+++ apps/frontend/src/pages/TerminosYCondiciones.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { AuthLayout } from "../components/layout/AuthLayout";
+
+const TerminosYCondiciones: React.FC = () => {
+  return (
+  <AuthLayout>
+    
+  </AuthLayout>
+
+  );
+};
+
+export default TerminosYCondiciones;
