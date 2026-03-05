f698c13d768ad715ef9b1548ea1a3b28be56cd88
nely <candezmr2@gmail.com>
2026-03-05 15:16:32 +0100

    puliendo estilos según documentación de UX/UI

--- apps/frontend/src/pages/MatchNutriList.tsx
+++ apps/frontend/src/pages/MatchNutriList.tsx
@@ -43,18 +43,18 @@ const MatchNutriList: React.FC = () => {
   return (
     <AppLayout>
       <article className="pl-4 border-b border-[#7ECD43] pb-2 mt-4">
-        <h2 className="text-[2em] font-bold mb-2">Listado de Nutricionistas</h2>
-        <p className="text-[1.25em]">Estos nutricionistas tienen mas compatibilidad con lo que estás buscando.</p>
+        <h2 className="text-[1.25em] leading-[1.75em] font-bold mb-2">Listado de Nutricionistas</h2>
+        <p className="text-[0.875em] leading-[1.375em]">Estos nutricionistas tienen mas compatibilidad con lo que estás buscando.</p>
       </article>
-      <p className="ml-4 mb-12">* Las recomendaciones se basan en la información proporcionada y no sustituyen evualuación médica</p>
+      <p className="text-[0.813em] leading-[1.25em] ml-4 mb-12">* Las recomendaciones se basan en la información proporcionada y no sustituyen evualuación médica</p>
       {nutricionistas.map((n: any) => (
         <div key={n.id} className="bg-white shadow-sm border-gray-300 border-b-4 border-x-2 mb-4 mx-4 rounded-2xl">
           <div className="flex items-start justify-between p-4">
             <img src={n.profile_picture_url || nutricionista} alt="foto perfil nutricionista" className="w-[83px] h-[83px] rounded-full object-cover object-center bg-slate-100" />
             <article>
-              <h3 className="font-bold text-[1.25em]">Dra./Dr. {n.user?.name || "Sin nombre"}</h3>
-              <p className="text-gray-400">{n.bio || "Nutricionista"}</p>
-              <p className="text-gray-400">Matrícula: {n.license_number}</p>
+              <h3 className="font-semibold text-[1.125em] leading-[1.625em]">Dra./Dr. {n.user?.name || "Sin nombre"}</h3>
+              <p className="text-[#6B7280] text-[0.875em] leading-[1.375em]">{n.bio || "Nutricionista"}</p>
+              <p className="text-[#6B7280] text-[0.875em] leading-[1.375em]">Matrícula: {n.license_number}</p>
               {n.tags?.length > 0 && (
                 <div className="flex flex-wrap gap-1 mt-1">
                   {n.tags.map((tag: any) => (
